dbd9ddcc6e3da5ef059f611e75820103ce054942
soldochris <soldevilla.christian@gmail.com>
2020-05-19 02:28:00 -0500

    lesson 5 folder created, favicon added

---
+++ lesson5/js/scripts.js
@@ -0,0 +1,24 @@
+// Code for get date in a nice format
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
+let dateNum = date.getDate();
+let dayNum = date.getDay();
+
+let day;
+for (let i = 0; i < days.length - 1; i++) {
+    if (i == dayNum) {
+        day = days[i];
+    }
+}
+
+document.getElementById("currentDate").innerHTML = day + ", " + dateNum + " " + months[month] + " " + year + ".";
+
+// Code for responsive navigation
+const hamButton = document.querySelector('#hamBtn');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => { navigation.classList.toggle('appear') }, false);
