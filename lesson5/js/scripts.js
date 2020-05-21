5d499560bf050f5b9cfc70dbdb795a858e17e3dd
soldochris <soldevilla.christian@gmail.com>
2020-05-21 00:15:57 -0500

    testing aside

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -14,16 +14,15 @@ for (let i = 0; i < days.length - 1; i++) {
         day = days[i];
     }
 }
-
 document.getElementById("currentDate").innerHTML = day + ", " + dateNum + " " + months[month] + " " + year + ".";
 
 // Code for responsive navigation
 const hamButton = document.querySelector('#hamBtn');
 const navigation = document.querySelector('.navigation');
-
 hamButton.addEventListener('click', () => { navigation.classList.toggle('appear') }, false);
 
-if (day == "Friday"){
+// If the actual day is Friday displays the banner
+if (day == "Thursday"){
     let aside = document.getElementById("aside");
     aside.style.display = "block";
 }
