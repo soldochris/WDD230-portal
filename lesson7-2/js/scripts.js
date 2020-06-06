7e478343045c3bb0b69b0ac8a1bef528c2b86760
soldochris <soldevilla.christian@gmail.com>
2020-06-06 03:40:47 -0500

    js file fixed, day varible deleted

--- lesson7-2/js/scripts.js
+++ lesson7-2/js/scripts.js
@@ -6,15 +6,9 @@ let date = new Date();
 let year = date.getFullYear();
 let month = date.getMonth();
 let dateNum = date.getDate();
-let dayNum = date.getDay();
+let dayNum = parseInt(date.getDay());
 
-let day;
-for (let i = 0; i < days.length - 1; i++) {
-    if (i == dayNum) {
-        day = days[i];
-    }
-}
-document.getElementById("currentDate").innerHTML = day + ", " + dateNum + " " + months[month] + " " + year + ".";
+document.getElementById("currentDate").innerHTML = days[dayNum] + ", " + dateNum + " " + months[month] + " " + year + ".";
 
 // Code for responsive navigation
 const hamButton = document.querySelector('#hamBtn');
@@ -22,7 +16,7 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => { navigation.classList.toggle('appear') }, false);
 
 // If the actual day is Friday displays the banner
-if (day == "Friday"){
+if (dayNum == 5){
     let aside = document.getElementById("aside");
     aside.style.display = "block";
 }
