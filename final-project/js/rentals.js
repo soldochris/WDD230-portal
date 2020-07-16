faa832ea16c57f9be4febc1b1a880101d8ba5e19
soldochris <soldevilla.christian@gmail.com>
2020-07-16 00:20:46 -0500

    rentals json fullday property fixed

--- final-project/js/rentals.js
+++ final-project/js/rentals.js
@@ -6,6 +6,27 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const rentals = jsonObject['rentals'];
-    console.log(rentals);
+    rentals.forEach(rental => {
+      let card = document.createElement('div');
+      let name = document.createElement('h3');
+      let capacity = document.createElement('p');
+      let reservation = document.createElement('ul');
+      let reservHalf = document.createElement('li');
+      let reservFull = document.createElement('li');
+      let walkin = document.createElement('ul');
+
+      name.textContent = rental.name;
+      capacity.textContent = rental.maxPersons;
+      reservHalf.textContent = rental.Reservation[0].halfDay;
+      reservFull.textContent = rental.Reservation[0].fullDay;
+
+      card.appendChild(name);
+      card.appendChild(capacity);
+      card.appendChild(reservation);
+      reservation.appendChild(reservHalf);
+      reservation.appendChild(reservFull);
+
+      document.getElementById('rentalCards').appendChild(card);
+    });
 
   });
