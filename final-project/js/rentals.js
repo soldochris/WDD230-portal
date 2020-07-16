1503605d9b0c41bff71d4272d02ad822cf3a6a54
soldochris <soldevilla.christian@gmail.com>
2020-07-16 01:40:08 -0500

    rentals page ready for small views

--- final-project/js/rentals.js
+++ final-project/js/rentals.js
@@ -11,25 +11,35 @@ fetch(requestURL)
       let name = document.createElement('h3');
       let capacity = document.createElement('p');
       let reservation = document.createElement('ul');
+      let reservTitle = document.createElement('lh');
       let reservHalf = document.createElement('li');
       let reservFull = document.createElement('li');
       let walkin = document.createElement('ul');
+      let walkinTitle = document.createElement('lh');
       let walkinHalf = document.createElement('li');
       let walkinFull = document.createElement('li');
+      let image = document.createElement('img');
 
+      image.setAttribute('src', 'images/'+ rental.image);
+      image.setAttribute('alt', 'Photo of '+ rental.name);
       name.textContent = rental.name;
-      capacity.textContent = rental.maxPersons;
-      reservHalf.textContent = rental.reservation[0].halfDay;
-      reservFull.textContent = rental.reservation[0].fullDay;
-      walkinHalf.textContent = rental.walkin[0].halfDay;
-      walkinFull.textContent = rental.walkin[0].fullDay;
+      capacity.textContent = 'Max. Persons: ' + rental.maxPersons;
+      reservTitle.textContent = 'Reservation';
+      reservHalf.textContent = rental.reservation[0].halfDay + ' for Half Day';
+      reservFull.textContent = rental.reservation[0].fullDay + ' for Full Day';
+      walkinTitle.textContent = 'Walk-In';
+      walkinHalf.textContent = rental.walkin[0].halfDay + ' for Half Day';
+      walkinFull.textContent = rental.walkin[0].fullDay + ' for Full Day';
 
+      card.appendChild(image);
       card.appendChild(name);
       card.appendChild(capacity);
       card.appendChild(reservation);
+      reservation.appendChild(reservTitle);
       reservation.appendChild(reservHalf);
       reservation.appendChild(reservFull);
       card.appendChild(walkin);
+      walkin.appendChild(walkinTitle);
       walkin.appendChild(walkinHalf);
       walkin.appendChild(walkinFull);
 
